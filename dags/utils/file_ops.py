3d655667c713204084234713eb55f902b284fcb5
Felix Leblanc <felix.leblanc1305@gmail.com>
2019-03-30 19:09:26 -0400

    gcs to local path utils function

--- dags/utils/file_ops.py
+++ dags/utils/file_ops.py
@@ -20,5 +20,16 @@ def get_sub_folders_list(dir_path):
     """
     return glob(os.path.join(dir_path), '*', '')
 
+def gcs_path_to_local_path(images_path, gcs_path):
+     """
+     Convert a Google Cloud Storage link into a local path to get an image
+     :param image_path: Needed to find the images path within the container
+     :param gcs_path: GCS Link to be converted
+     :return: Converted local path to the image
+     """
+     split = gcs_path.split("/")
 
+     dataset = split[3]
+     image = split[4]
 
+     return os.path.join(images_path, dataset, image)
