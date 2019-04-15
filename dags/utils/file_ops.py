5e8cc0909c92f594bd7f38b1342db5b6a755470b
Lucas Mongrain <xeaprog@gmail.com>
2019-04-15 13:57:46 -0400

    add json to voc

--- dags/utils/file_ops.py
+++ dags/utils/file_ops.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from glob import glob
+import json
 
 
 def get_parent_folder_name(dir_path):
@@ -33,3 +34,19 @@ def gcs_path_to_local_path(images_path, gcs_path):
      image = split[4]
 
      return os.path.join(images_path, dataset, image)
+
+def concat_json(json_files, output_path):
+     """
+     concat multiples json into one
+    :param json_files: dictionnary of json file
+    :param output_path: file name of the resulting json file
+    :return: none
+    """
+
+     json_dict = []
+     with open(output_path, "w") as out:
+          for f in json_files:
+               with open(f, 'rb') as infile:
+                    data = json.load(infile)
+                    json_dict += data
+          json.dump(json_dict, out)
