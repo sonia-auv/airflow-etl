41823da680de4e05ccdcf93d1164f5b3ba16bb9f
Martin Gauthier <you@example.com>
2019-11-30 10:32:15 -0500

    Added start end shutdown script

--- shutdown.sh
+++ shutdown.sh
@@ -10,12 +10,11 @@
 # -- INSTRUCTIONS: ------------------------------------------------------------
 #
 # Execute:
-#   $ chmod u+x start.sh && ./start.sh --env dev
+#   $ chmod u+x shutdown.sh && ./shutdown.sh
 #
 # Options:
 #   -h, --help      output program instructions
 #   -v, --version   output program version
-#   -e, --env       set build environment variable (e.g: dev, prod)
 #
 # ------------------------------------------------------------------------------
 # | VARIABLES                                                                  |
@@ -29,7 +28,7 @@ BOLD="$(tput bold)"
 RESET="$(tput sgr0)"
 
 VERSION="1.0.0"
-PROGRAM="start"
+PROGRAM="shutdown"
 
 export AIRFLOW_DOCKER_IMAGE_NAME="soniaauvets/airflow-ros-tensorflow"
 export AIRFLOW_DOCKER_IMAGE_TAG="$(cat VERSION)"
@@ -63,42 +62,32 @@ e_warning() {
     printf "${ORANGE}![WARNING]:${RESET}" "$@"
 }
 
-e_airflow_container_starting() {
+e_airflow_container_shutting_down(){
     echo "#########################################################################"
     echo
-    e_success "Launching sonia-auv airflow docker containers"
+    echo "Shutting down sonia-auv aiflow docker containers"
 }
 
-e_airflow_container_started() {
+e_airflow_container_shutted_down(){
     echo "#########################################################################"
     echo
-    e_success "Airflow containers have ${GREEN}${BOLD}STARTED${GREEN}${BOLD}"
-}
-
-e_airflow_image_pulling() {
-    echo "#########################################################################"
-    echo
-    e_success "Pulling image '${AIRFLOW_DOCKER_IMAGE_NAME}' using tag '${AIRFLOW_DOCKER_IMAGE_TAG}'"
+    echo "Airflow containers have ${RED}${BOLD}STOPPED${RED}${BOLD}"
 }
 
 # ------------------------------------------------------------------------------
 # | MAIN FUNCTIONS                                                             |
 # ------------------------------------------------------------------------------
 
-context(){
-    e_info "Build context:${BUILD_ENV}"
-}
-
 help() {
 
 cat <<EOT
 
 ------------------------------------------------------------------------------
-Start - DESCRIPTION
+Shutdown - DESCRIPTION
 ------------------------------------------------------------------------------
 
-Usage: ./start.sh
-Example: ./start.sh
+Usage: ./shutdown.sh
+Example: ./shutdown.sh
 
 
 Options:
@@ -110,59 +99,10 @@ EOT
 
 }
 
-parseArgs() {
-    BUILD_ENV=${1}
-    if [[ -z ${BUILD_ENV} ]]; then
-        e_error "BUILD_ENV argument must be defined on calling ! i.e : ./start.sh [BUILD_ENV]"
-    elif [[ "${BUILD_ENV}" -ne "dev" || "${BUILD_ENV}" -ne "prod" ]]; then
-        e_error "BUILD_ENV argument value must be dev or prod"
-    fi
-}
-
-requiredFolderExist() {
-    declare -a List=(
-                 "${CURRENT_DIR}/dags"
-                 "${CURRENT_DIR}/data"
-                 "${CURRENT_DIR}/logs"
-                 "${CURRENT_DIR}/plugins"
-                )
-
-    for folder_path in "${List[@]}"
-    do
-        if [ ! -d ${folder_path} ]; then
-            mkdir ${folder_path}
-            echo "${folder_path}....${GREEN}${BOLD}CREATED!${GREEN}${BOLD}${RESET}"
-        else
-            echo "${folder_path}....${GREEN}${BOLD}FOUND!${GREEN}${BOLD}${RESET}"
-        fi
-
-    done
-}
-
-start(){
-    parseArgs $*
-    context
-    requiredFolderExist || e_error "Error while creating folder required by airflow on localhost"
-    if [[ ${BUILD_ENV} == 'prod' ]]; then
-        start_prod || e_error "Error while starting '${AIRFLOW_DOCKER_IMAGE_NAME}'"
-    else
-        start_dev || e_error "Error while starting '${AIRFLOW_DOCKER_IMAGE_NAME}'"
-    fi
-
-    e_airflow_container_started
-}
-
-start_dev() {
-    e_airflow_container_starting
-    AIRFLOW_DAG_DIR=${AIRFLOW_DAG_DIR} HOST_ROOT_FOLDER=${CURRENT_DIR}  docker-compose -f ${DOCKER_DIR}/docker-compose.yml up -d || e_error "Error while starting '${AIRFLOW_DOCKER_IMAGE_NAME}'"
-}
-
-start_prod() {
-    e_airflow_image_pulling
-    docker pull ${AIRFLOW_DOCKER_IMAGE_NAME}:${AIRFLOW_DOCKER_IMAGE_TAG} ||e_error "Error pulling '${AIRLFLOW_DOCKER_IMAGE_NAME}'"
-
-    e_airflow_container_starting
-    AIRFLOW_DAG_DIR=${AIRFLOW_DAG_DIR} HOST_ROOT_FOLDER=${CURRENT_DIR}  docker-compose -f ${DOCKER_DIR}/docker-compose.yml -f ${DOCKER_DIR}/docker-compose-prod.yml up -d|| e_error "Error while starting '${AIRFLOW_DOCKER_IMAGE_NAME}'"
+shutdown() {
+   e_airflow_container_shutting_down
+   IRFLOW_DAG_DIR=${AIRFLOW_DAG_DIR} HOST_ROOT_FOLDER=${CURRENT_DIR} docker-compose -f ${DOCKER_DIR}/docker-compose.yml down || e_error "Error while shutting down sonia-auv airflow docker containers"
+    e_airflow_container_shutted_down
 }
 
 version() {
@@ -179,10 +119,8 @@ main() {
         help ${1}
     elif [[ "${1}" == "-v" || "${1}" == "--version" ]]; then
         version ${1}
-    elif [[ "${1}" == "-e" || "${1}" == "--env" ]]; then
-        start ${2}
     else
-        e_error "You must provide an build enviroment using the -e or --env flag"
+        shutdown
     fi
 
 }
