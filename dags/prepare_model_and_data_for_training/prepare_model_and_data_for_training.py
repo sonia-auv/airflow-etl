c2e049f95f038fa5532fad69b4c99b35ce4fcab1
Martin Gauthier <you@example.com>
2019-11-30 22:12:25 -0500

    Testing labemap check

--- dags/prepare_model_and_data_for_training/prepare_model_and_data_for_training.py
+++ dags/prepare_model_and_data_for_training/prepare_model_and_data_for_training.py
@@ -139,6 +139,8 @@ def compare_label_map_file(base_tf_record_folder, video_source):
         reference_label_map = label_maps[0]
         labelmap_match = True
         for label_map in label_maps:
+            print(label_map)
+            print(reference_label_map)
             if filecmp(label_map, reference_label_map):
                 print(f"[ MATCH ] | LabelMap:{label_map} ")
             else:
