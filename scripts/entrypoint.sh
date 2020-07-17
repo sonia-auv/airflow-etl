7c8392ee263c92310420b2a6bf65726dfeff00f8
Martin Gauthier <gauthiermartin86@gmail.com>
2020-07-17 14:19:09 -0400

    Review entrypoint

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+TRY_LOOP="20"
+
+
+# Wait for Postresql
+if [ "$1" = "webserver" ] || [ "$1" = "worker" ] || [ "$1" = "scheduler" ] ; then
+  i=0
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT >/dev/null 2>&1 < /dev/null; do
+    i=$((i+1))
+    if [ "$1" = "webserver" ]; then
+      echo "$(date) - Waiting for database @ ${POSTGRES_HOST}:${POSTGRES_PORT}... $i/$TRY_LOOP"
+      if [ $i -ge $TRY_LOOP ]; then
+        echo "$(date) - ${POSTGRES_HOST}:${POSTGRES_PORT} could not reached, giving up"
+        exit 1
+      fi
+    fi
+    sleep 10
+  done
+fi
+
+echo "Initialize database..."
+airflow upgradedb
+exec airflow webserver &
+exec airflow scheduler
